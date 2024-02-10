4aa66f4e7c4613bdd4134041f8af67f4cc22a178
Nate Kite <nrk67593@uga.edu>
2024-02-10 14:47:53 -0500

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,30 @@ import React from "react";
 
 import "./Navbar.css";
 
-const Navbar = () => {
+// bold_page: should be: "home", "chats", "events", "connect", or "none"
+// signed_in: whether or not the user is currently signed in (boolean)
+// profile_pic: if the user is signed in, URI to their profile pic, otherwise null
+const Navbar = (props) => {
 	return (
-		<nav class="navbar navbar-expand-lg navbar-bg sticky-top ps-4 pe-5 navbar-text">
+		<nav class="navbar navbar-expand-lg navbar-bg sticky-top ps-3 pe-4 navbar-text">
 			<div class="container-sm justify-content-start">
-				<img src={require('../assets/logo.png')} alt="logo" height="75" width="75"></img>
-				<a href="/" class="nav-link nav-text-big ps-3">GlobaLink</a>
+				<img src={require('../assets/logo.png')} alt="logo" height="45" width="45" class="my-1"></img>
+				<a href="/" class="nav-link ps-3 pt-1"><span style={{fontSize: "28px", fontWeight: 550}}>GlobaLink</span></a>
 			</div>
 			<div class="container-fluid">
-				<a class="nav-link" href="/home">Home</a> 
-				<a class="nav-link" href="/chat">Chats</a>
-				<a class="nav-link" href="/events">Events</a>
-				<a class="nav-link" href="/connect">Connect</a>
+				<a class="nav-link" href="/home" style={props.bold_page === 'home' ? {fontWeight:550} : {}}>Home</a> 
+				<a class="nav-link" href="/chat" style={props.bold_page === 'chats' ? {fontWeight:550} : {}}>Chats</a>
+				<a class="nav-link" href="/events" style={props.bold_page === 'events' ? {fontWeight:550} : {}}>Events</a>
+				<a class="nav-link" href="/connect" style={props.bold_page === 'connect' ? {fontWeight:550} : {}}>Connect</a>
 				<div class="v-line"></div>
-				<a class="nav-link" href="/">Join Now</a>
-				<button class="btn btn-primary" href="/">Sign In</button>
+				{props.signed_in == "true"
+					? <a class="nav-link" href="/">My Profile</a>
+					: <a class="nav-link" href="/">Join Now</a>
+				}
+				{props.signed_in == "true"
+					? <img src={props.profile_pic} alt="profile" height="40" width="40"></img>
+					: <a class="nav-link" href="/" style={{fontWeight: 550}}>Sign In</a>
+				}
 			</div>
 		</nav>
 	);
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,8 +6,9 @@ import Navbar from "../components/Navbar"
 const About = () => {
 	return (
 		<div>
-			<h1>About Page</h1>
-			<Navbar></Navbar>
+			<Navbar bold_page="home" signed_in="true"></Navbar>
+			<h1>This is the about page.</h1>
+			<p>Sphinx of black quartz, judge my vow.</p>
 		</div>
 	);
 };
