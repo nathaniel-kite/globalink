ccab9f9d23fb863b9b91efc41dd80f640b13cc92
Nate Kite <nrk67593@uga.edu>
2024-02-09 19:23:21 -0500

    First commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://nathankite:Z2wGCHQmazL4s53H@cluster0.tr1trie.mongodb.net/?retryWrites=true&w=majority;"
+/* Replace <password> with your database password */
+
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
