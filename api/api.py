6bf5bf7a6759cee4e0e28fc02554b0d7ac86d9f5
Nate Kite <nrk67593@uga.edu>
2024-02-11 02:50:09 -0500

    Connected create-profile with backend

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import json
-from utils import get_all_mentors
+from utils import get_all_mentors, insert_one
 
 app = Flask(__name__)
 
@@ -12,3 +12,7 @@ def test():
 def get_all_mentors_endpoint():
 	mentor_list = {"mentors": get_all_mentors()}
 	return json.dumps(mentor_list, default=str)
+
+@app.route("/api/register", methods=["POST"])
+def register():
+	return {"message": "Not implemented."}
--- src/pages/CreateProfile.jsx
+++ src/pages/CreateProfile.jsx
@@ -5,26 +5,76 @@ import './CreateProfile.css'
 
 const CreateProfile = () => {
 
+	function handleSubmission() {
+		const form = document.getElementById('profileForm');
+			form.addEventListener('submit', (event) => {
+				event.preventDefault();
+			});
+
+		let submissionData = {
+			username: form.username.value,
+			password: form.password.value,
+			first_name: form.first_name.value,
+			last_name: form.last_name.value,
+			origin_country: form.origin_country,
+			languages: [
+				{
+					name: form.language_1.value,
+					proficiency: "Native"
+				},
+				{
+					name: form.language_2.value,
+					proficiency: form.language_2_proficiency.value,
+				},
+				{
+					name: form.language_3.value,
+					proficiency: form.language_3_proficiency.value,
+				}
+			],
+			education: form.education.value,
+			current_location: form.current_location.value,
+			description: form.description.value
+		}
+
+		fetch('/api/register', {
+			method: 'POST',
+			body: submissionData
+		})
+		.then(res => res.json())
+		.then(data => alert(data));
+	}
+
 	return (
 		<div style={{background: '#F0F8FF', height: '100%'}}>
 			<Navbar bold_page="profile" signed_in="false"></Navbar>
-			<form action="/api/create-profile" method="post" class="default-text-create-profile">
+			<form class="default-text-create-profile" id="profileForm">
 				<div class="container py-3" style={{maxWidth: '1000px'}}>
 					<h1 class="px-5 py-3">Create Mentee Profile</h1>
 					<div class="question-card d-flex flex-column px-5 py-3 mb-4">
 						<h3 class="mb-4">Account Information</h3>
-						<label for="country" class="form-label">Username</label>
+						<label for="username" class="form-label">Username</label>
 						<input type="text" class="form-control mb-3" id="username" name="username"></input>
-						<label for="country" class="form-label">Password</label>
+						<label for="password" class="form-label">Password</label>
 						<input type="password" class="form-control mb-3" id="password" name="password"></input>
 					</div>
 					<div class="question-card d-flex flex-column px-5 py-3">
 						<h3 class="mb-4">Personal Information</h3>
-						<label for="country" class="form-label">Name</label>
-						<input type="text" class="form-control mb-3" id="name" name="name"></input>
-						<label for="country" class="form-label">Country of Origin</label>
+
+						<div class="flex-row d-flex">
+							<div class="flex-column d-flex flex-fill pe-3">
+								<label for="first_name" class="form-label">First Name</label>
+								<input type="text" class="form-control mb-3" id="first_name" name="first_name"></input>
+							</div>
+							<div class="flex-column d-flex flex-fill">
+								<label for="last_name" class="form-label">Last Name</label>
+								<input type="text" class="form-control mb-3" id="last_name" name="last_name"></input>
+							</div>
+						</div>
+
+						
+						<label for="origin_country" class="form-label">Country of Origin</label>
 						<input type="text" class="form-control mb-3" id="origin_country" name="origin_country"></input>
-						<label for="country" class="form-label">Languages</label>
+						<label for="language_1" class="form-label">Languages</label>
 						<input type="text" class="form-control mb-3" id="language_1" name="language_1" placeholder="Native Language (Required)"></input>
 						<div class="flex-row d-flex">
 							<input type="text" class="form-control mb-3 flex-fill me-3" id="language_2" name="language_2" placeholder="Second Language (Optional)"></input>
@@ -48,13 +98,13 @@ const CreateProfile = () => {
 						</div>
 						<label for="education" class="form-label">Education</label>
 						<input type="text" class="form-control mb-3" id="education" name="education" placeholder="Your highest level of education (eg. 'B.S. Computer Science')"></input>
-						<label for="current-location" class="form-label">Current Location</label>
-						<input type="text" class="form-control mb-3" id="education" name="education" placeholder="(eg. Atlanta, Georgia)"></input>
-						<label for="current-location" class="form-label">Description</label>
-						<textarea class="form-control mb-3" id="education" name="description" placeholder="Tell us a little about yourself. What are you hoping for help with?"></textarea>
+						<label for="current_location" class="form-label">Current Location</label>
+						<input type="text" class="form-control mb-3" id="current_location" name="current_location" placeholder="(eg. Atlanta, Georgia)"></input>
+						<label for="current_location" class="form-label">Description</label>
+						<textarea class="form-control mb-3" id="description" name="description" placeholder="Tell us a little about yourself. What are you hoping for help with?"></textarea>
 					</div>
-					<div class='d-flex justify-content-center my-4'>
-						<button class='btn btn-success default-text-create-profile'>Create Profile</button>
+					<div class="d-flex justify-content-center my-4">
+						<button onClick={handleSubmission} class="btn btn-success default-text-create-profile">Create Profile</button>
 					</div>
 				</div>
 			</form>
