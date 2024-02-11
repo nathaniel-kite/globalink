0ac4542d2a1b00f2289cf0f486308fe531d1ecb1
Sammy Beard <91227894+sam8beard@users.noreply.github.com>
2024-02-10 20:22:21 -0500

    Edited schemas

--- api/utils.py
+++ api/utils.py
@@ -20,8 +20,18 @@ mentor_collection = db['mentor']
 
 # FOR TESTING PURPOSES 
 def main(): 
+<<<<<<< Updated upstream
     clear_mentees(mentee_collection) 
     clear_mentors(mentor_collection) 
+=======
+    sofia_languages = [{'name': 'Spanish', 'proficiency': 4}, {'name': 'English', 'proficiency': 3}]
+    carlos_languages = [{'name': 'Spanish', 'proficiency': 4}, {'name': 'English', 'proficiency': 3}]
+    chris_languages = [{'name': 'Zulu', 'proficiency': 4}, {'name': 'English', 'proficiency': 3}]
+    markus_languages = [{'name': 'German', 'proficiency': 4}, {'name': 'English', 'proficiency': 3}, {'name': 'Swedish', 'proficiency': '2'}, {'name': 'Mandarin', 'proficiency': '1'}]
+
+    clear_mentees() 
+    clear_mentors() 
+>>>>>>> Stashed changes
     test_mentee_data = {'first_name': 'sammy', 'last_name': 'beard', 'username': 'sam8beard', 'password': '1234', 'profilePic': 'test'}
     test_mentor_data1 = {'name': 'Nate Kite', 'origin_country': 'Slovakia', 'description': 'Eyes of emerald that shimmer under the sunlight', 'current_rate': 50, 
                         'languages': ['spanish, proficient', 'english, fluent'], 'sklls': ['talking', 'coding']}
@@ -29,12 +39,28 @@ def main():
                         'languages': ['german, proficient', 'english, fluent'], 'sklls': ['cooking', 'coding']}
     test_mentor_data3 = {'name': 'Venn Reddy', 'origin_country': 'Japan', 'description': 'Big boy with a big heart', 'current_rate': 10, 
                         'languages': ['japanese, proficient', 'english, fluent'], 'sklls': ['bad mitton', 'coding']}
+    test_mentor_data3 = {'name': 'Venn Reddy', 'origin_country': 'Japan', 'description': 'Big boy with a big heart', 'current_role': 'Product Manager at Amazon', 
+                        'languages': ['japanese, proficient', 'english, fluent'], 'sklls': ['bad mitton', 'coding']}
+    
+    sofia_data = {'name': 'Sofia Gonzalez', 'origin_country': 'Bogota, Columbia', 
+                  'description': 'Hi, my name is Sofia Gonzalez and I am a project manager with 25+ years of experience in the corporate world. I would love to chat!', 
+                  'current_role': 'Project Manager at Microsoft', 'languages': sofia_languages, 'skills': ['Retirement Funds', 'Networking', 'Management Roles', 'Finance Management'], 
+                  'profile_pic': 'https://cdn.discordapp.com/attachments/1043592304910422027/1206038730482257970/image_10.png?ex=65da8e18&is=65c81918&hm=08bd8dd63fbc87ddc8de9ac264e971237432e9740be944df2286233b51229a91&',
+                   'current_location': 'Atlanta, Georgia'}
+    
+    carlos_data = {'name': 'Carlos Vargas', 'origin_country': 'Mexico City, Mexico', 
+                   'description': 'Hello, I would love to chat about anything from career work, acclimating to the US, or basketball. Feel free to reach out!',
+                   'current_role': 'Creative Directory at ESPN', 'languages': carlos_languages,
+                   'skills': ['Work-Life Balance', 'Networking', 'Adjusting to Culture', 'Visa Issues'], 
+                   'profile_pic': 'https://cdn.discordapp.com/attachments/1043592304910422027/1206039229352644628/unsplash_SJvDxw0azqw.png?ex=65da8e8f&is=65c8198f&hm=66f4344c20ebae8e5cb834d9ded9776ad4c95c8594728a4646cd36ed6b686668&',
+                   'current_location': 'Atlanta, Georgia'}
+    
     mentee_collection.insert_one(test_mentee_data)
-    mentor_collection.insert_one(test_mentor_data1)
-    mentor_collection.insert_one(test_mentor_data2)
-    mentor_collection.insert_one(test_mentor_data3)
-    search_mentee_by_name('sammy','beard')
-    search_mentor_by_language(['spanish, proficient', 'english, fluent']) 
+    # mentor_collection.insert_one(test_mentor_data1)
+    mentor_collection.insert_one(sofia_data)
+    mentor_collection.insert_one(carlos_data)
+    # search_mentee_by_name('sammy','beard')
+    # search_mentor_by_language(['spanish, proficient', 'english, fluent']) 
 
     mentors = get_all_mentors(mentor_collection)
     pprint.pprint(mentors)
